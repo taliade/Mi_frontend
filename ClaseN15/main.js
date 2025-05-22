10a5f57b14fd6401aa22e03b03e20db70ad77430
Talia Rodrigues <taliraodrigues@gmail.com>
2025-05-21 21:06:13 -0300

    Segunda clase de js

---
+++ ClaseN15/main.js
@@ -0,0 +1,42 @@
+// Operadores Matematicos 
+
+//Suma  
+
+let suma = 15 + 7;
+
+console.log(suma);
+
+let resta = 10 - 2; 
+
+console.log(resta);
+
+let division = 45 / 5;
+console.log(division);
+
+let total = division - resta;
+
+console.log(total);
+
+let modulo = 10 % 3;
+console.log(modulo);
+
+let incrementar = 10;
+   incrementar++; // +1
+   console.log(incrementar);
+
+let decrementar = 20;
+
+    decrementar--;
+
+    console.log(decrementar);
+/// El prompt es una Funcion de ventana emergente 
+let nombre = prompt("¿cual es tu nombre?");  
+
+console.log("Hola  soy " + nombre);
+
+alert(" Bienvenida " + nombre);
+
+let namee = "Taliaa";
+let comida = "Pizza";
+
+console.log( ` Hola, mi nombre es ${namee}   y me gusta la ${comida}`);
---
+++ ClaseN15/practica/main.js
@@ -0,0 +1,29 @@
+
+
+let mensaje = prompt( " Ingresa tu nombre y apellido: ");
+
+console.log(mensaje);
+
+let datosMios = prompt("Ingrese su nombre");
+
+console.log(datosMios)
+
+let trabajoAct = prompt("¿Cual es tu trabajo Actual?");
+
+console.log(trabajoAct);
+
+let frase = ` Hola Soy  ${datosMios} ,  Actualmente estoy trabajando como  ${trabajoAct}`;
+
+console.log(frase);
+//Nombre, color preferido, comida
+///favorita, mejor película, trabajo actual
+
+let actividad = alert(mensaje); //Actividad 3
+
+
+let numero = prompt("Ingrese un numero: ");
+if (numero % 2 === 0){
+    console.log("Es par");
+}else {
+    console.log("Es impar");
+}
